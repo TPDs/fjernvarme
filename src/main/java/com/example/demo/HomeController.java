14309206651c937a68c448d4a2bfb32b44f261bb
TPD <mig.mic@gmail.com>
2020-04-20 11:28:53 +0200

    Calculator

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 
@@ -12,5 +15,42 @@ public String index() {
     return "Bin";
 
 }
+
+    @GetMapping("/actors")
+    public String actors() {
+        return "Actors";
+
+    }
+
+    @GetMapping("/movies")
+    public String movies() {
+        return "Movies";
+
+    }
+
+    @GetMapping("/calculaor")
+    public String calculator() {
+        return "calculator";
+
+    }
+
+    @PostMapping("/result")
+    public String result(WebRequest wr, Model model) {
+        String tal1 = wr.getParameter("value1");
+        String tal2 = wr.getParameter("value2");
+
+        assert tal1 != null;
+        int tallet1 = Integer.parseInt(tal1);
+        assert tal2 != null;
+        int tallet2 = Integer.parseInt(tal2);
+
+        int result = tallet1 + tallet2;
+
+        model.addAttribute("sumresult",result);
+
+        return "result";
+
+    }
+
 
 }
